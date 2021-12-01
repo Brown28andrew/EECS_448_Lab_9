937e56b3db5746ab13b4afb7c612a40946630dc1
Andrew Brown <83791452+Brown28andrew@users.noreply.github.com>
2021-11-30 22:40:28 -0600

    Update mulTable.php

--- mulTable.php
+++ mulTable.php
@@ -2,46 +2,28 @@
   error_reporting(E_ALL);
   ini_set("display_errors", 1);
 
-  function makeRow($x){
-    for($i = 1; $i <= 100; $i++){
-      $x[$i] = $i;
-    }
-  }
-
-  function tableRow($x, $y){
-    for($i = 0; $i < 100; $i++){
-      $z[$i] = $x * $y[$i+1];
-    }
-    return $z;
-  }
   echo "Multiplication Table: <br>";
   echo "<table border= 'black 2px'>";
-    $row = [ ];
-    makeRow($row);
     for($i = 0; $i <= 100; $i++){
+      echo "<tr>";
       if($i == 0){
-        echo "<tr>";
-          echo "<td> </td>";
-          for($j = 1; $j <= 100; $j++){
-                echo "<td>";
-                echo row[$j];
-                echo "</td>";
-          }
-        echo "</tr>";
-      }else{
-        $newRow = tableRow($i, $row);
-        echo "<tr>";
-          echo "<td>"
-          echo $i;
+        echo "<td> </td>";
+        for($j = 1; $j <= 100; $j++){
+          echo "<td>";
+          echo $j;
           echo "</td>";
-          for($z = 0; $z < 99; $z++){
-            echo "<td>";
-            echo newRow[$z];
-           echo "</td>";
-         }
-       echo "</tr>";
-     }
-   }
+        }
+      }else{
+        echo "<td>";
+        echo $i;
+        echo "</td>";
+        for($j = 1; $j <= 100; $j++){
+          echo "<td>";
+          echo $i * $j;
+          echo "</td>";
+        }
+      }
+        echo "</tr>";
+    }
   echo "</table>";
-
 ?>
